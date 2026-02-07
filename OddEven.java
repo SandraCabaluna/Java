d8b86d17fafed8c75180596999555319eaf3de17
Sandra Cabaluna <cabalunasandra543@gmail.com>
2026-02-07 18:50:30 +0800

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,15 @@
+import java.outil.*;
+public class OddEven {
+  static Scanner input = new Scanner(System.in);
+  public class static void main(String[] sandra) {
+    int N = 0;
+
+      System.out.print("Enter an integer number: ");
+      N = input.nextInt();
+
+      if((N % 2) == 0)
+      System.out.println("You entered even number: ");
+      else 
+      System.out.println("You entered odd number: ");
+  }
+}
