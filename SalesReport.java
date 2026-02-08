535972dc6fda7508f8b87781fe2a3d3e5359a054
Sandra Cabaluna <cabalunasandra543@gmail.com>
2026-02-08 21:42:58 +0800

    Create SalesReport.java

---
+++ SalesReport.java
@@ -0,0 +1,17 @@
+class SalesReport {
+    public static void main (String[] sandra) {
+        int[][] sales {
+            {1200, 1500, 1700},
+            {1000, 1100, 1300},
+            {900, 1200, 1600}
+        };
+
+        for (int i = 0; i < sales.length; i++) {
+            int total = 0;
+            for (int j = 0; j < sales[i].length; j++) {
+                total += sales[i][j];
+            }      
+            System.out.println("Total sales for store: " + (i+1) + ":" + total);      
+        }
+    }
+}
