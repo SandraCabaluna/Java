c6e79d2d57e417bafe567234a11088b66f49581f
Sandra Cabaluna <cabalunasandra543@gmail.com>
2026-02-08 21:56:38 +0800

    Create SwitchDemo.java

---
+++ SwitchDemo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class SwitchDemo {
+    public static void main(String[] sandra) {
+      Scanner input = new Scanner(System.in);
+
+
+int days=0;
+switch(days){
+            System.out.println("Enter number 1 - 7: ");
+
+    case 1: System.out.println("Sunday");
+    break;
+
+    case 2: System.out.println("Monday");
+    break;
+
+    case 3: System.out.println("Tuesday");
+    break;
+
+    case 4: System.out.println("Wednesday");
+    break;
+
+    case 5: System.out.println("Thursday");
+    break;
+
+    case 6: System.out.println("Friday");
+    break;
+
+    case 7: System.out.println("Saturday");
+    break;
+
+    default: System.out.println("Invalid");
+}
+}
+}
